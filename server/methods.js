c8364593f739eb4763217e53d7da9367e5b1064c
Pauli B <pauli.buccini@gmail.com>
2014-09-01 00:20:50 +0700

    added case for day

--- server/methods.js
+++ server/methods.js
@@ -14,10 +14,39 @@ Meteor.methods({
   },
   addDishToAttending: function (mealId, dishId) {
     var meal = Meals.findOne({_id: mealId})
-    var users = Meteor.users.find({chef: {$ne: true}})
+    var day = moment(meal.day, 'YYYY-MM-DD').format('dddd').toLowerCase();
+    var dayHelper = 0;
+    switch (day) {
+      case 'monday':
+        dayHelper = 0;
+      break;
+      case 'tuesday':
+        dayHelper = 1;
+      break;
+      case 'wenesday':
+        dayHelper = 2;
+      break;
+      case 'thursday':
+        dayHelper = 3;
+      break;
+      case 'friday':
+        dayHelper = 4;
+      break;
+      case 'saturday':
+        dayHelper = 5;
+      break;
+      case 'sunday':
+        dayHelper = 6;
+      break;
+    }
+
+    var users = Meteor.users.find({chef: {$ne: true}});
+
+
+
     users.forEach(function (user) {
-      if (user) {
-        
+      if (user && user.profile.days) {
+
       }
     })
   }
