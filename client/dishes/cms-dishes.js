94f37161943a4f461d3fe6e28fff6948d508cc50
Pauli B <pauli.buccini@gmail.com>
2014-08-31 18:36:03 +0700

    added almost works edit

--- client/dishes/cms-dishes.js
+++ client/dishes/cms-dishes.js
@@ -1,8 +1,38 @@
 Template.CMSdishes.helpers({
-
+  dishes: function () {
+    return Dishes.find()
+  }
 });
 
 Template.CMSdishes.events({
+  'click .js-addNewDish': function () {
+    Dishes.insert({
+      _id: Random.id(),
+      thaiName:'',
+      englishName: '',
+      picture: '',
+      timesOrdered: 0,
+      offeredBy: []
+    }, function (err, id) {
+      Session.set('editingDishId', id)
+    })
+  },
+  'click .js-editDish': function (e, t) {
+    Session.set('editingDishId', this._id)
+    Session.toggle("newDishOpen");
+  }
+})
+
+Template.addDish.helpers({
+  openState: function () {
+    return Session.equals('newDishOpen', true) ? 'is-open' : '';
+  },
+  editableDish: function () {
+    return Session.get('editingDishId') ? Dishes.findOne({_id: Session.get('editingDishId')}) : {}
+  }
+})
+
+Template.addDish.events({
   'change .js-uploadPicture': function (e, t) {
     var file = e.target.files[0];
     if (file)
@@ -14,23 +44,7 @@ Template.CMSdishes.events({
         
       }
       fr.readAsDataURL(file);
-  }
-})
-
-
-Template.CMSdishes.events({
-  'click .js-editDish': function (e, t) {
-    Session.toggle("newDishOpen");
-  }
-});
-
-Template.addDish.helpers({
-  openState: function () {
-    return Session.equals('newDishOpen', true) ? 'is-open' : '';
-  }
-})
-
-Template.addDish.events({
+  },
   'click .js-closeModal': function (e, t) {
     Session.set("newDishOpen", false);
   }
