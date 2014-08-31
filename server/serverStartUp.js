5f5a2ca846f42088627714253590a2c1c8db80fb
Pauli B <pauli.buccini@gmail.com>
2014-08-31 17:24:44 +0700

    bug

---
+++ server/serverStartUp.js
@@ -0,0 +1,83 @@
+Meteor.startup(function () {
+  // Days.remove({})
+  // Meteor.users.remove({})
+  // Dishes.remove({})
+  // Attending.remove({})
+  // Meals.remove({});
+if (Meteor.users.find().count() == 0) {
+
+  var today = new Date();
+  var todayFormated = moment(today).format('YYYY-MM-DD');
+
+
+   Meals.insert({
+      _id: "Meal0001",
+      day: todayFormated,
+      time: '10:00',
+      owner: 'notSet',
+      dishes: [{dish: 'Dish0001', dishOptions: []}, 
+      {dish: 'Dish0002', dishOptions: ['chicken']}]
+    })
+
+  var users = [
+      {name:"Sa",email:"chef@tapfuse.io",roles:['cook']},
+    ];
+
+  _.each(users, function (user) {
+    var id;
+
+    id = Accounts.createUser({
+      email: user.email,
+      password: "apple1",
+      profile: {fullName: user.name}
+    });
+
+    if (user.roles.length > 0) {
+      // Need _id of existing user record so this call must come 
+      // after `Accounts.createUser` or `Accounts.onCreate`
+      Roles.addUsersToRoles(id, user.roles);
+    }
+
+    Attending.insert({
+      day: moment(new Date()).format('YYYY-MM-DD'),
+      owner: id,
+      mealId: 'Meal0001',
+      myMeals: [{dish: 'Dish0001', eating: true}, {dish: 'Dish0002', eating: true}],
+      disabled: false
+    })
+
+  });
+
+
+  if (Days.find().count() === 0) {
+    
+    Days.insert({
+      date: today,
+      day: todayFormated
+    });
+
+    Dishes.insert({
+      _id: 'Dish0001',
+      thaiName:'ต้มยำ',
+      englishName: 'Tom Yam',
+      picture: 'http://imgur.com/WDgcp6h.jpg',
+      timesOrdered: 0,
+      offeredBy: []
+    });
+
+    Dishes.insert({
+      _id: 'Dish0002',
+      thaiName:'ต้มยำ',
+      englishName: 'Pad Thai',
+      picture: 'http://www.qualitylivinginchiangmai.com/wp-content/uploads/2012/08/pad-thai.jpg',
+      timesOrdered: 0,
+      offeredBy: []
+    });
+
+
+
+
+  }
+}
+
+})
