ffe93c1ea65f3e89cdaf0c191a9282f5cfd8fc1e
Pauli B <pauli.buccini@gmail.com>
2014-08-31 16:25:57 +0700

    fix for disable eat

--- client/users/user-profile.js
+++ client/users/user-profile.js
@@ -32,9 +32,9 @@ Template.profileSettings.events({
   },
   'click .js-disableEating': function () {
     var modifier = {$set: {}};
-    modifier.$set["profile.days." + this.day + '.' + this.mealType +".notEating"] = false;
+    modifier.$set["profile.days." + this.day + '.' + this.mealType +".notEating"] = true;
     modifier.$set["profile.days." + this.day + '.' + this.mealType +".fruit"] = false;
-    modifier.$set["profile.days." + this.day + '.' + this.mealType +".food"] = true;
+    modifier.$set["profile.days." + this.day + '.' + this.mealType +".food"] = false;
     Meteor.users.update({_id: Meteor.userId()}, modifier)
   },
 })
